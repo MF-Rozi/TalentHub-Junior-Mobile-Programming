1888407f6bdb588d5bb7a81ab1ad52e38509ec36
mfrozi <m.fakhrulrozi10@gmail.com>
2023-07-06 00:18:43 +0700

    init: create android project

---
+++ Day-2/JuniorMobileProgrammerTask/app/src/main/java/com/mfrozi/junior_mobile_programmer_task/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.mfrozi.junior_mobile_programmer_task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
